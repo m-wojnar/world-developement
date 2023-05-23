ba8ae8ee47ca167d95ac1d4788e7e5fe24556deb
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2023-05-23 15:45:04 +0200

    Change to log scale

--- data_exploration/features_importance.py
+++ data_exploration/features_importance.py
@@ -13,12 +13,12 @@ df_test = df[year == 2020]
 with open('../data/preprocessed/series.json', 'r') as f:
     series = json.load(f)
 
-gdp_pcap = np.exp(df_train['GDP.PCAP.LOG'])
+gdp_pcap_log = df_train['GDP.PCAP.LOG']
 
 print(f'{"Significance":^15}|{"Series":^85}|{"Coefficient":^20}|{"Intercept":^20}')
 print('-' * 140)
 
 for i, col in enumerate(df.columns[:-2], start=1):
     if col in series:
-        lm = LinearRegression().fit(df_train[col].values.reshape(-1, 1), gdp_pcap)
+        lm = LinearRegression().fit(df_train[col].values.reshape(-1, 1), gdp_pcap_log)
         print(f'{i:>10}.    |{series[col]:85}|{lm.coef_[0]:^20.4f}|{lm.intercept_:^20.4f}')
