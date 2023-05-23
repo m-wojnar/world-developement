cd66fc47cacf7910cc18285881e0398ca2a2c9e2
adam <adam.przywieczerski@gmail.com>
2023-05-23 14:04:04 +0200

    divide into 2 functions

--- data/database_operations.py
+++ data/database_operations.py
@@ -34,32 +34,46 @@ def load_json_from_database(database: str, table: str):
   return res
 
 
-def load_all_data(folder: str, database_name: str):
+def save_all_data_to_db(folder: str, database_name: str):
   database_path = folder + '/' + database_name
   if os.path.exists(database_path):
     os.remove(database_path)
-  
+
   # csv to sql table
   data_csv, table_name = f'{folder}/data.csv', f'{folder}_data'
   csv_to_table(data_csv, database_path, table_name)
-  data = load_csv_from_database(database_path, table_name)
-  
+
   # json to sql table
   years_json, table_name, columns = f'{folder}/years.json', f'{folder}_years', ['year', 'year_string']
   json_to_table(f'{years_json}', database_path, table_name, columns)
-  years = load_json_from_database(database_path, table_name)
 
   series_json, table_name, columns = f'{folder}/series.json', f'{folder}_series', ['series', 'description']
   json_to_table(f'{series_json}', database_path, table_name, columns)
-  series = load_json_from_database(database_path, table_name)
 
   countries_json, table_name, columns = f'{folder}/countries.json', f'{folder}_countries', ['country_code', 'country']
   json_to_table(f'{countries_json}', database_path, table_name, columns)
+
+
+def load_all_data_from_db(folder: str, database_name: str):
+  database_path = folder + '/' + database_name
+  
+  # csv from sql table
+  table_name = f'{folder}_data'
+  data = load_csv_from_database(database_path, table_name)
+
+  table_name = f'{folder}_years'
+  years = load_json_from_database(database_path, table_name)
+  
+  table_name = f'{folder}_series'
+  series = load_json_from_database(database_path, table_name)
+
+  table_name = f'{folder}_countries'
   countries = load_json_from_database(database_path, table_name)
 
   return data, years, series, countries
 
   
-  
+
 if __name__ == "__main__":
-  load_all_data('preprocessed', 'world_data.db')
+  save_all_data_to_db('preprocessed', 'world_data.db')
+  load_all_data_from_db('preprocessed', 'world_data.db')
