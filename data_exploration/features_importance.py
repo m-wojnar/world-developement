8f8501e95f021aeab8ca814ac5d125bf72b323d7
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2023-05-23 15:54:32 +0200

    Add script for GDP prediction

--- data_exploration/features_importance.py
+++ data_exploration/features_importance.py
@@ -1,24 +1,23 @@
 import json
 
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import LinearRegression
 
 
 df = pd.read_csv('../data/preprocessed/data.csv')
+df.pop('country')
+
 year = df.pop('year')
 df_train = df[year == 2019]
-df_test = df[year == 2020]
 
 with open('../data/preprocessed/series.json', 'r') as f:
     series = json.load(f)
 
-gdp_pcap_log = df_train['GDP.PCAP.LOG']
+gdp_pcap_log = df_train.pop('GDP.PCAP.LOG')
 
 print(f'{"Significance":^15}|{"Series":^85}|{"Coefficient":^20}|{"Intercept":^20}')
 print('-' * 140)
 
-for i, col in enumerate(df.columns[:-2], start=1):
-    if col in series:
-        lm = LinearRegression().fit(df_train[col].values.reshape(-1, 1), gdp_pcap_log)
-        print(f'{i:>10}.    |{series[col]:85}|{lm.coef_[0]:^20.4f}|{lm.intercept_:^20.4f}')
+for i, col in enumerate(df_train.columns, start=1):
+    lm = LinearRegression().fit(df_train[col].values.reshape(-1, 1), gdp_pcap_log)
+    print(f'{i:>10}.    |{series[col]:85}|{lm.coef_[0]:^20.4f}|{lm.intercept_:^20.4f}')
